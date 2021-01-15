24f94f04c2f4a175f729e43ac132c287e9a3852c
RyanAdamGonzalez1996 <ryangonzalezportfolio@gmail.com>
2021-01-14 20:46:14 -0500

    Psuedo Code.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,29 @@
 // @TODO: YOUR CODE HERE!
+
+// Data Columns
+//id, state, abbr(state), poverty
+//povertyMoe, age, ageMoe, income
+//incomeMoe, healthcare, healthcareLow
+//healthcareHigh, obesity, obesityLow,
+//obesityHigh, smokes, smokesLow, smokesHigh
+// -0.385218228
+
+
+// Figure out how to run this with 
+// `python -m http.server`
+
+// read in csv data
+
+// Assign variables to data values
+
+// Create the object for the space needed for the plot
+
+// Create the plot
+// Situate axes and labels to the left and bottom of chart
+
+// Populate the plot (with state abbreviations in circle)
+
+// maybe (mouseover event to increase size of dot)
+
+// Optional Assignment: Look at final class assignment
+
